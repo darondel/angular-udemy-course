b17c7a74961f391739104e994a48be3302c5b41a
Damien Arondel <damien.arondel@icloud.com>
2018-08-15 13:14:37 +0200

    #2 Set up the routing of Recipes feature module

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { RecipesComponent } from "./recipes.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { AuthGuard } from "../auth/auth-guard.service";
+import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
+import { RecipeResolver } from "./recipe-resolver.service";
+
+const routes: Routes = [
+  {
+    path: 'recipes', component: RecipesComponent, children: [
+      {path: '', component: RecipeStartComponent},
+      {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
+      {path: ':id', component: RecipeDetailsComponent, resolve: {recipe: RecipeResolver}},
+      {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard], resolve: {recipe: RecipeResolver}}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+}
