bdcd886833f44ee2b1d1fa2ceacf638df9b76f74
Damien Arondel <damien.arondel@icloud.com>
2018-09-09 15:26:48 +0200

    #7 Set up the Recipe store

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { RecipesRoutingModule } from "./recipes-routing.module";
-import { SharedModule } from "../shared/shared.module";
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
-import { RecipesComponent } from "./recipes.component";
-import { RecipeListComponent } from "./recipe-list/recipe-list.component";
-import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
-import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
-import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeEffects } from './store/effects/recipe.effects';
+import { recipesReducers } from './store/reducers/recipes.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    StoreModule.forFeature('recipes', recipesReducers),
+    EffectsModule.forFeature([RecipeEffects]),
     RecipesRoutingModule,
     SharedModule
   ],
