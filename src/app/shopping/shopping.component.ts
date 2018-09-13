347479901b11f1255449f6677cf973eaf0ea6aec
Damien Arondel <damien.arondel@icloud.com>
2018-09-13 22:00:49 +0200

    #13 Let Shopping items (dis)appear one after another, with a fade effect

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 
 import { select, Store } from '@ngrx/store';
 import { Dictionary } from '@ngrx/entity';
 
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { Ingredient } from './store/models/ingredient.model';
 import { getIngredientEntities, ShoppingFeatureState } from './store/reducers/shopping.reducer';
@@ -11,18 +13,31 @@ import { getIngredientEntities, ShoppingFeatureState } from './store/reducers/sh
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
-  styleUrls: ['./shopping.component.css']
+  styleUrls: ['./shopping.component.css'],
+  animations: [
+    trigger('shoppingListAnimation', [
+      transition('* => *', [
+        query(':enter', [
+          style({opacity: 0}),
+          stagger(100, animate('.5s', style({opacity: 1})))
+        ], {optional: true}),
+        query(':leave', stagger(100, animate('.5s', style({opacity: 1}))), {optional: true})
+      ])
+    ])
+  ]
 })
 export class ShoppingComponent implements OnInit {
 
   ingredients: Observable<Dictionary<Ingredient>>;
+  ingredientsTotal: number;
 
   constructor(private store: Store<ShoppingFeatureState>) {
   }
 
   ngOnInit() {
     this.ingredients = this.store.pipe(
-      select(getIngredientEntities)
+      select(getIngredientEntities),
+      tap(ingredients => this.ingredientsTotal = Object.entries(ingredients).length)
     );
   }
 
