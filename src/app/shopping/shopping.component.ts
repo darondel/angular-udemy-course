89ca5515980093c6c6bb917d30c2e08cd6344e2f
Damien Arondel <damien.arondel@icloud.com>
2018-09-13 22:06:32 +0200

    #13 Fix the disappear opacity value and set up a 'trackBy' function

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -21,7 +21,7 @@ import { getIngredientEntities, ShoppingFeatureState } from './store/reducers/sh
           style({opacity: 0}),
           stagger(100, animate('.5s', style({opacity: 1})))
         ], {optional: true}),
-        query(':leave', stagger(100, animate('.5s', style({opacity: 1}))), {optional: true})
+        query(':leave', stagger(100, animate('.5s', style({opacity: 0}))), {optional: true})
       ])
     ])
   ]
@@ -39,6 +39,17 @@ export class ShoppingComponent implements OnInit {
       select(getIngredientEntities),
       tap(ingredients => this.ingredientsTotal = Object.entries(ingredients).length)
     );
+  }
+
+  /**
+   * Track ingredients in an iterable.
+   *
+   * @param index the index of the ingredient in the iterable
+   * @param ingredient the ingredient
+   * @return the track id of the ingredient
+   */
+  trackIngredient(index: number, ingredient: { key: string, value: Ingredient }): any {
+    return ingredient.key;
   }
 
 }
